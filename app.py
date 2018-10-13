72619331275226917f933a40d40913f56d88283c
Megha Guggari <mguggari@terpmail.umd.edu>
2018-10-13 01:24:23 -0500

    front page

--- app.py
+++ app.py
@@ -2,6 +2,14 @@ from flask import Flask
 from flask import render_template
 app = Flask(__name__)
 
+user = {'userName': 'Rohit', 
+		'childName': 'Megha'}
+
+child = {'childName': 'Megha',
+		 'grade': 4,
+		 'englishGrade': "D",
+		 'mathGrade': "E",
+		 'scienceGrade': "C"}
 
 @app.route("/")
 def hello():
@@ -16,3 +24,13 @@ def test():
 @app.route("/character")
 def test2():
     return "characters page"
+    
+
+@app.route("/ParentPortal")
+def test3():
+	return render_template("/concept-clean/index.html", user=user)
+
+
+@app.route("/Enrollment")
+def Enrollment():
+	return render_template("/concept-clean/form.html", user=user, child=child)
