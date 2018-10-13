120215de167c8ec066f08dba0ef787c4d03fa8fe
Megha Guggari <mguggari@terpmail.umd.edu>
2018-10-13 01:23:42 -0500

    front page

--- layout/scripts/supersized/supersized.setup.js
+++ layout/scripts/supersized/supersized.setup.js
@@ -1,3 +1,4 @@
+
 jQuery(function ($) {
     $.supersized({
         // Functionality
@@ -8,9 +9,9 @@ jQuery(function ($) {
         // Components							
 
         slides: [ // Slideshow Images
-        {image: 'images/slider/1.gif'}, 
-		{image: 'images/slider/2.gif'}, 
-		{image: 'images/slider/3.gif'}
+        {image: 'images/slider/math.jpg'}, 
+		{image: 'images/slider/grammar_cute.jpg'}, 
+		{image: 'images/slider/math_cute2.jpg'}
 		]
     });
 });
